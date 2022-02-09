9c1a712c02de907b41fa049e29be86b41b71644f
0scarW <90772493+0scarW@users.noreply.github.com>
2022-02-09 12:18:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-const myString = "Hello, world!";
-console.log(myString);
+let saldo = prompt("Ange ett tal ");
+
+console.log(saldo)
+
+const järn = 100;
+
+const silver = 300;
+
+let guld = 1000;
+
+while (true) {
+    if (saldo >= 1000) {
+        console.log('Du kan köpa "${saldo//100}" guldsmycken');
+    }
+    if (saldo >= 300) {
+        console.log('Du kan köpa "${saldo//100}" silversmycken');
+    }
+    if (saldo >= 100) {
+        console.log('Du kan köpa "${saldo//100}" järnsmycken');
+    }
+    if (saldo < 100) {
+        console.log("Du har inte råd med några smycken")
+        break
+    }
+}
+
